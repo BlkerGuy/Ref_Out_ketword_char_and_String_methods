137cf23727bdc30fa127e0bed57f67687af34978
BlkerGuy <2020.gafarov@gmail.com>
2023-02-02 04:35:23 +0400

    Add project files.

---
+++ Ref_Out_ketword_char_and_String_methods/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Xml.Linq;
+
+namespace Ref_Out_ketword_char_and_String_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            //-Verilmiş int array-ə verilmiş int dəyəri əlavə edən metod.Misalçün metodu çağırıb array elementimi və 5 dəyəri göndərsəm,
+            //arrayim dəyişilməli və arrayin içində əvəlki elementləri,
+            //əlavə olaraq son indexində isə 5 elementi olmalıdır və arrayin uzunluğu 1 vahid artmış olmalıdır.(metod geriye dəyər qaytarmır)
+
+
+            //int[] nums = new int[] { 12, 8, 17 };
+
+            //addValueToArr(ref nums,5);
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine(nums[i]);
+            //}
+
+
+            // Task 2
+            //-Verilmiş yazıda rəqəm olub olmadığını tapan metod.Göndərilən yazıda rəqəm varsa true yoxdursa false qaytarır.
+            //Console.WriteLine(findDigit("Azerbaycan Torpaqlari artiq  azadd1ir!"));
+
+            // Task 3 
+            //-Verilmiş yazıdaki sözlərin sayanı tapan metod.
+            //Boşluqla bir birindən ayrılmış bütün ifadələri söz kimi qəbul edin.Və hər söz arasında bir boşluq varmış kimi hesab edin.
+
+            string str = "Salam necesen? Hikmet?";
+
+            //Console.WriteLine(str.Length);
+            Console.WriteLine(findLengthofSentence(str));
+
+            // Mellim serti basa dusduyum iki sekilde yazmaga calisdim :
+        }
+
+        //  Method of Task 1
+        //static void addValueToArr(ref int[] numArr, int num)
+        //{
+        //    int[] newArr = new int[numArr.Length + 1];
+
+        //    for (int i = 0; i < numArr.Length; i++)
+        //    {
+        //        newArr[i] = numArr[i];
+        //    }
+        //    newArr[newArr.Length - 1] = num;
+        //    numArr = newArr;
+        //}
+
+        // Method of Task 2
+
+        //  static bool findDigit(string sentenc)
+        //{
+        //    for (int i = 0; i < sentenc.Length; i++)
+        //    {
+        //        if (char.IsDigit(sentenc[i]))
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        // Method of Task 3
+        static int findLengthofSentence(string sentence)
+        {
+            //return sentence.Length;
+
+            string words =sentence.Trim();
+            string newStr = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (char.IsLetter(words[i]))
+                {
+                    newStr += words[i];
+                    newStr += ' ';
+                }
+            }
+            return newStr.Length;
+        }
+
+
+    }
+}
---
+++ inClassPractica/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace inClassPractica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(whiteSpaceIndex(" Hikmet"));
+            //Console.WriteLine(charCount("Hikmet"));
+            //int[] numArr = new int[] { 2,4,10};
+            //Console.WriteLine(findArr(numArr,1));
+
+            //Console.WriteLine(PositivMinus(-5));
+            //string newStr = "Salam necesen, Abbas?";
+            //rmStrSpace(ref newStr);
+            //Console.WriteLine(newStr);
+
+
+
+
+
+
+            Console.WriteLine(char.IsDigit('2'));
+
+            Console.WriteLine(char.IsLetter('c'));
+
+            Console.WriteLine(char.IsLetter('4'));
+
+            Console.WriteLine(char.IsLetterOrDigit('@'));
+
+            Console.WriteLine("Hikmet".ToLower());
+            Console.WriteLine("Hikmet".ToUpper());
+
+
+            string word = "Hikmet";
+
+            var newWord = word.ToUpper();
+            newWord = word.ToLower();
+
+            Console.WriteLine(newWord);
+
+            Console.WriteLine(newWord.Contains("km"));
+
+            Console.WriteLine(newWord.IndexOf('m'));
+            Console.WriteLine(newWord.LastIndexOf('m'));
+            Console.WriteLine(newWord.IndexOf("tem"));
+
+
+            word = "   salamm Hikmett  ";
+            Console.WriteLine(word.TrimStart());
+            Console.WriteLine(word.TrimEnd());
+            Console.WriteLine(word.Trim());
+
+            var index = word.IndexOf("necesen");
+            var word2 = word.Substring(index, 4);
+
+            word2 = word.Replace("salam", "hello");
+
+            Console.WriteLine("baslayirmi? " + word.StartsWith("Hello P"));
+            Console.WriteLine("bitirmi? " + word.EndsWith("up!"));
+
+
+            word = "Hello P138 group!";
+            var arr = word.Split(" ");
+
+            string[] names = { "Hikmet", "Abbas", "Tofiq" };
+            var result = String.Join(" - ", names);
+
+        }
+
+        //static int whiteSpaceIndex(string word)
+        //{
+        //    //int index = 0;
+        //    for (int i = 0; i <word.Length; i++)
+        //    {
+        //        if (word[i]==' ')
+        //        {
+        //            return i;
+        //        }
+        //    }
+        //    return -1;
+        //}
+
+        //static int charCount(string word)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        return count += word.Length;
+        //    }
+        //    return 0;
+        //}
+
+        //static int[] findArr(int[] nums,int arrLength)
+        //{
+        //    int[] newArr = new int[nums.Length + 1];
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        newArr[i] += nums[i];
+        //        if (newArr[i]==nums[i])
+        //        {
+        //          newArr[arrLength.Le] = 25;
+        //            Console.WriteLine(newArr[3]);
+        //        }
+        //    }
+        //    return newArr;////   DUZELT BUNU <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        //}
+
+        //static void rmStrSpace(ref string word)
+        //{
+        //    string newStr = "";
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        if (word[i] != ' ')
+        //        {
+        //            newStr += word[i];
+        //        }
+        //    }
+        //    word = newStr;
+        //}
+
+        //static int PositivMinus(int nums)
+        //{
+        //     return   nums *= -1;
+        //}
+    }
+}
